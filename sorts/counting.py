9fb9075c72e97a69711fd801ffc817896c653615
Abtinmy <abtinmahyar@gmail.com>
2021-08-12 18:12:52 +0430

    fix: remove duplicate codes and add info for counting sort

--- sorts/counting.py
+++ sorts/counting.py
@@ -1,7 +1,19 @@
+"""
+    *** Time Complexity ***
+    * Best case : O(N+K) -> which K is the range of input
+    * Worst case : O(N+K)
+    * Average : O(N+K)
+    
+    * Space complexity = O(N+K)
+    * Not In-Place sort
+    * Stable sort
+"""
+
+
 def countingsort(arr):
     res = arr.copy()
     out = [0] * len(res)
-    count = [0] * 256
+    count = [0] * 257 # 256 elements for 256 chars and last one for storing the size of res 
 
     for char in arr:
         count[ord(char)] += 1
@@ -12,20 +24,17 @@ def countingsort(arr):
     for i in reversed(range(1, 256)):
         count[i] = count[i - 1]
 
-    count[0] = 0
+    count[0], count[256] = 0, len(res)
     counter = 0
 
     for i in range(256):
-        if i == 255:
-            for j in range(count[i], len(res)):
-                 out[counter] = chr(i)
-                 counter += 1
-        else:
-            for j in range(count[i], count[i + 1]):
-                out[counter] = chr(i)
-                counter += 1
+        for j in range(count[i], count[i + 1]):
+            out[counter] = chr(i)
+            counter += 1
         if counter == len(res):
             break
             
     return out
+
     
+#TODO: Stable counting sort, support negative numbers
