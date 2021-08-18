b0b2d2a6b3f1d2f7b9d82104b567628a2eacd3fd
Abtinmy <abtinmahyar@gmail.com>
2021-08-19 00:45:56 +0430

    feat: added counting sort and it's test for supporting negative numbers

--- sorts/counting.py
+++ sorts/counting.py
@@ -11,8 +11,7 @@
 
 
 def countingsort(arr):
-    res = arr.copy()
-    out = [0] * len(res)
+    out = [0] * len(arr)
     count = [0] * 257 # 256 elements for 256 chars and last one for storing the size of res 
 
     for char in arr:
@@ -24,17 +23,37 @@ def countingsort(arr):
     for i in reversed(range(1, 256)):
         count[i] = count[i - 1]
 
-    count[0], count[256] = 0, len(res)
+    count[0], count[256] = 0, len(arr)
     counter = 0
 
     for i in range(256):
         for j in range(count[i], count[i + 1]):
             out[counter] = chr(i)
             counter += 1
-        if counter == len(res):
+        if counter == len(arr):
             break
             
     return out
 
-    
-#TODO: Stable counting sort, support negative numbers
+def countingsort_neg_num(arr):
+    max_ele = max(arr)
+    min_ele = min(arr)
+    range_ele = max_ele - min_ele + 1
+
+    count = [0] * range_ele
+    out = [0] * len(arr)
+
+    for i in range(len(arr)):
+        count[arr[i] - min_ele] += 1
+
+    for i in range(1, len(count)):
+        count[i] += count[i - 1]
+
+    for i in reversed(range(len(arr))):
+        out[count[arr[i] - min_ele] - 1] = arr[i]
+        count[arr[i] - min_ele] -= 1
+
+    return out
+
+
+# Using for finding mode and median of an array with limited range    
--- sorts/tests/test_counting.py
+++ sorts/tests/test_counting.py
@@ -1,5 +1,8 @@
 import unittest
-from sorts.counting import countingsort
+from sorts.counting import (
+    countingsort,
+    countingsort_neg_num
+)
 import random
 
 
@@ -9,9 +12,14 @@ class TestCounting(unittest.TestCase):
         self.sample_list = []
         for i in range(10):
             self.sample_list.append(chr(random.randint(0, 256)))
+        self.neg_list = random.sample(range(-10, 10), 10)
 
     def test_countingsort(self):
         result = countingsort(self.sample_list)
         self.assertListEqual(result, sorted(self.sample_list))
+
+    def test_countingsort_neg_num(self):
+        result = countingsort_neg_num(self.neg_list)
+        self.assertListEqual(result, sorted(self.neg_list))
 
    
